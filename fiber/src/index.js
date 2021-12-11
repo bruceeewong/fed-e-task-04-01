c5ac4ac5f26c24a75bf9a3cf1f7c4e4e6f936ea3
bruskiwang <bruskiwang@tencent.com>
2021-12-11 16:48:25 +0800

    feat: 处理节点更新

--- fiber/src/index.js
+++ fiber/src/index.js
@@ -2,14 +2,24 @@ import React, { render, Component } from "./react";
 
 const root = document.getElementById("root");
 
-// const jsx = (
-//   <div>
-//     <p>Hello React</p>
-//     <p>Goodbye React</p>
-//   </div>
-// );
-//
-// render(jsx, root);
+const jsx = (
+  <div>
+    <p>Hello React</p>
+    <p>Goodbye React</p>
+  </div>
+);
+
+render(jsx, root);
+setTimeout(() => {
+  const jsx = (
+    <div>
+      <p>奥利给</p>
+      <p>Goodbye React</p>
+    </div>
+  );
+
+  render(jsx, root);
+}, 2000);
 
 // class Greeting extends Component {
 //   constructor(props) {
@@ -23,8 +33,8 @@ const root = document.getElementById("root");
 
 // render(<Greeting title={'Hello'} />, root);
 
-function Greeting(props) {
-  return <div>{props.title} hahaha</div>;
-}
-
-render(<Greeting title={"Hello"} />, root);
+// function Greeting(props) {
+//   return <div>{props.title} hahaha</div>;
+// }
+//
+// render(<Greeting title={"Hello"} />, root);
--- fiber/src/react/reconciliation/index.js
+++ fiber/src/react/reconciliation/index.js
@@ -1,4 +1,5 @@
 import { arrified, createStateNode, createTaskQueue, getTag } from "../Misc";
+import { updateNodeElement } from "../DOM";
 
 const taskQueue = createTaskQueue();
 
@@ -18,6 +19,7 @@ function getFirstTask() {
     tag: "host_root",
     effects: [],
     child: null,
+    alternate: task.dom.__rootFiberContainer, // 备份旧的fiber
   };
 }
 
@@ -27,27 +29,62 @@ function reconcileChildren(fiber, children) {
 
   let index = 0;
   let numberOfElements = arrifiedChildren.length;
-  let element = null;
-  let newFiber = null;
-  let prevFiber = null;
+  let element = null; //
+  let newFiber = null; // 新的 fiber 对象
+  let prevFiber = null; // 上一个兄弟 fiber 对象
+  let alternate = null; // 备份节点
+
+  if (fiber.alternate && fiber.alternate.child) {
+    alternate = fiber.alternate.child; // 只有第一个子节点是child
+  }
 
   while (index < numberOfElements) {
     element = arrifiedChildren[index];
-    newFiber = {
-      type: element.type,
-      props: element.props,
-      tag: getTag(element),
-      effects: [],
-      effectTag: "placement",
-      parent: fiber,
-    };
 
-    newFiber.stateNode = createStateNode(newFiber);
+    if (element && !alternate) {
+      // 初始渲染
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        tag: getTag(element),
+        effects: [],
+        effectTag: "placement",
+        parent: fiber,
+      };
+
+      newFiber.stateNode = createStateNode(newFiber);
+    } else if (element && alternate) {
+      // 更新操作
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        tag: getTag(element),
+        effects: [],
+        effectTag: "update",
+        parent: fiber,
+        alternate, // 存备份fiber
+      };
+
+      // 判断新旧fiber类型是否相等
+      if (element.type === alternate.type) {
+        // 类型相同，复用dom
+        newFiber.stateNode = alternate.stateNode;
+      } else {
+        // 类型不同，直接替换dom
+        newFiber.stateNode = createStateNode(newFiber);
+      }
+    }
 
     if (index === 0) {
       fiber.child = newFiber;
     } else {
       prevFiber.sibling = newFiber;
+    }
+
+    if (alternate && alternate.sibling) {
+      alternate = alternate.sibling; // 除了第一个子节点之外的子节点都是 sibling
+    } else {
+      alternate = null;
     }
 
     prevFiber = newFiber;
@@ -93,6 +130,7 @@ function executeTask(fiber) {
  * @param fiber 最外层节点的Fiber对象
  */
 function commitAllWork(fiber) {
+  // 循环 effects 数组，构建 DOM 节点树
   fiber.effects.forEach((item) => {
     if (item.effectTag === "placement") {
       let fiber = item;
@@ -106,8 +144,23 @@ function commitAllWork(fiber) {
       if (fiber.tag === "host_component") {
         parentFiber.stateNode.appendChild(fiber.stateNode); // 将fiber对应的dom追加至根节点的dom
       }
+    } else if (item.effectTag === "update") {
+      // 更新
+      if (item.type === item.alternate.type) {
+        // 节点类型相同
+        updateNodeElement(item.stateNode, item, item.alternate); // TODO: 只能处理元素节点，不能处理文本节点
+      } else {
+        // 节点类型不同
+        item.parent.stateNode.replaceChild(
+          item.stateNode, // 新节点
+          item.alternate.stateNode // 旧节点
+        );
+      }
     }
   });
+
+  // 备份旧的 fiber 节点对象
+  fiber.stateNode.__rootFiberContainer = fiber;
 }
 
 function workLoop(deadline) {
